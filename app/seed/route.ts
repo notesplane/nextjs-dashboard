c6f056bb9c48fb7377992b2f9b44b51c69143fe4
admin_win <6188871+notesplane@users.noreply.github.com>
2024-12-02 19:34:21 +0900

    Revert "Update route.ts"
    This reverts commit fd32c1805298ae5ff9d92d6dfba9cf58adb2fd30.

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,6 +102,10 @@ async function seedRevenue() {
 }
 
 export async function GET() {
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
   try {
     await client.sql`BEGIN`;
     await seedUsers();
@@ -114,11 +118,5 @@ export async function GET() {
   } catch (error) {
     await client.sql`ROLLBACK`;
     return Response.json({ error }, { status: 500 });
-  }  
-} 
-
-
-//return Response.json({
-//  message:
-//  'Uncomment this file and remove this line. You can delete this file when you are finished.',
-//});
+  }
+}
